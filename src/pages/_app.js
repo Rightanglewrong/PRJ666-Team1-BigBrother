5f065607149795ec0e20c7c6fa5e4afeee5debf2
STZ0214 <steven.zhang.0214@gmail.com>
2024-09-11 16:21:48 -0400

    install tailwindcss

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,8 +2,7 @@ import NavBar from '../components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '../app/globals.css';
-
-
+import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
   return (
