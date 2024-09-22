8525848c5f9da9c94d52e217391bd3c8b07b0c4c
achoi28 <116921949+achoi28@users.noreply.github.com>
2024-09-22 19:46:30 -0400

    dashboard test
    dashboard test code

---
+++ src/aws-exports.js
@@ -0,0 +1,15 @@
+const awsconfig = {
+    aws_project_region: process.env.NEXT_PUBLIC_AWS_REGION,  // Your AWS region
+    aws_cognito_region: process.env.NEXT_PUBLIC_AWS_REGION,  // Your Cognito region (usually the same as the project region)
+    aws_user_pools_id: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID,  // Your Cognito User Pool ID
+    aws_user_pools_web_client_id: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID,  // Your Cognito User Pool App Client ID
+    oauth: {
+        domain: 'https://big-brother2-pool.auth.us-east-1.amazoncognito.com/login?client_id=69n73oh4j7t8ou9f1u9sqs8e3g&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fbig-brother-psi.vercel.app%2Fdashboard',  // Your Cognito Hosted UI domain
+        scope: ['email', 'openid', 'profile'],  // Scopes you need for authentication
+        redirectSignIn: 'https://big-brother-psi.vercel.app/dashboard',  // URL to redirect after successful sign-in
+        redirectSignOut: 'https://big-brother-psi.vercel.app/',  // URL to redirect after successful sign-out
+        responseType: 'code',  // Response type for OAuth2 (code or token)
+    }
+};
+
+export default awsconfig;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import NavBar from '../components/NavBar';
 import '../app/globals.css';
+import { Amplify } from 'aws-amplify';
+import awsconfig from '../aws-exports';  // Import your aws-exports.js configuration
+
+Amplify.configure(awsconfig);
+
+
 
 function MyApp({ Component, pageProps }) {
   return (
