17aec929ce8b16dfc4d3d0ba61b935569f2f621b
Rightanglewrong <bennyyang996648@hotmail.com>
2024-10-13 03:45:43 -0400

    Created newsletter index

---
+++ src/pages/newsletter/index.js
@@ -0,0 +1,75 @@
+// src/pages/newsletter/index.js
+
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { getCurrentUser } from "@/utils/api";
+import { getAllNewsletters } from "@/utils/newsletterAPI";
+
+export default function NewsletterIndex() {
+  const [newsletters, setNewsletters] = useState([]);
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      setMessage("User is not authenticated");
+      return;
+    }
+
+    async function fetchNewsletters() {
+      try {
+        const user = await getCurrentUser(token);
+        const newsletters = await getAllNewsletters(token, user.locationID);
+        // Sort newsletters by `createdAt` in descending order (newest first)
+        const sortedNewsletters = newsletters.sort(
+          (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+        );
+        setNewsletters(sortedNewsletters);
+        setLoading(false);
+      } catch (error) {
+        if (error.message == "404: No Newsletters currently") {
+          setMessage("");
+        } else {
+          setMessage(`${error}`);
+        }
+        setLoading(false);
+      }
+    }
+
+    fetchNewsletters();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>; // Show loading state while fetching user details
+  }
+
+  return (
+    <div>
+      <h1>Newsletters</h1>
+      {message && <p>{message}</p>}
+
+      {newsletters.length > 0 ? (
+        newsletters.map((newsletter) => (
+          <div key={newsletter.newsletterID}>
+            <h3>{newsletter.title}</h3>
+            <p>{newsletter.content}</p>
+            <p>{newsletter.createdAt}</p>
+            <div>
+              <Link href={`/newsletter/${newsletter.newsletterID}`}>
+                <button>Expand</button>
+              </Link>
+            </div>
+            <br></br>
+          </div>
+        ))
+      ) : (
+        <p>No newsletters found.</p>
+      )}
+      <br></br>
+      <Link href="/newsletter/create">
+        <button>Create New Newsletter</button>
+      </Link>
+    </div>
+  );
+}
