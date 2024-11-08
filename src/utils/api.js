4d5d2b497ed792146b57093099afd3f91df40473
Rightanglewrong <bennyyang996648@hotmail.com>
2024-11-08 14:03:42 -0500

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,20 +1,20 @@
 const BACKEND_URL = "https://big-brother-be-3d6ad173758c.herokuapp.com/";
 
 export async function updateUserProfile(updatedData) {
-  const token = localStorage.getItem('token'); // Retrieve token from localStorage
+  const token = localStorage.getItem("token"); // Retrieve token from localStorage
 
   const response = await fetch(`${BACKEND_URL}auth/update`, {
     method: "PUT",
     headers: {
-      'Authorization': `Bearer ${token}`, // Pass JWT token for authentication
-      'Content-Type': 'application/json'
+      Authorization: `Bearer ${token}`, // Pass JWT token for authentication
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(updatedData) // Send updated user data
+    body: JSON.stringify(updatedData), // Send updated user data
   });
 
   if (!response.ok) {
     const error = await response.json();
-    throw new Error(error.message || 'Failed to update profile');
+    throw new Error(error.message || "Failed to update profile");
   }
 
   return await response.json(); // Return the success message or updated data
@@ -22,7 +22,7 @@ export async function updateUserProfile(updatedData) {
 
 // Get the current user's details using the token
 export async function getCurrentUser() {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
 
   if (!token) {
     throw new Error("No token found");
@@ -32,48 +32,49 @@ export async function getCurrentUser() {
     const response = await fetch(`${BACKEND_URL}auth/me`, {
       method: "GET",
       headers: {
-        'Authorization': `Bearer ${token}`, // Pass JWT in headers
-        'Content-Type': 'application/json',
-      }
+        Authorization: `Bearer ${token}`, // Pass JWT in headers
+        "Content-Type": "application/json",
+      },
     });
 
     if (!response.ok) {
       if (response.status === 401) {
-        throw new Error('Unauthorized access. Please log in again.');
+        throw new Error("Unauthorized access. Please log in again.");
       } else if (response.status === 404) {
-        throw new Error('User not found. Please check your account.');
+        throw new Error("User not found. Please check your account.");
       } else {
-        throw new Error('Failed to fetch user details. Please try again later.');
+        throw new Error(
+          "Failed to fetch user details. Please try again later."
+        );
       }
     }
 
     const userData = await response.json();
-    return userData;  // Return user details from backend
+    return userData; // Return user details from backend
   } catch (error) {
-    console.error('Error fetching user details:', error);
+    console.error("Error fetching user details:", error);
     throw new Error(error.message);
   }
 }
-
 
 export async function login(email, password) {
   try {
     const response = await fetch(`${BACKEND_URL}auth/login`, {
       method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ email, password }),
     });
 
     if (!response.ok) {
-      throw new Error('Login failed');
+      throw new Error("Login failed");
     }
 
     const data = await response.json();
-    return data;  // Should return token or error message
+    return data; // Should return token or error message
   } catch (error) {
-    console.error('Error logging in:', error);
+    console.error("Error logging in:", error);
     return { error: error.message };
   }
 }
@@ -82,49 +83,51 @@ export async function signup(signupData) {
   const response = await fetch(`${BACKEND_URL}auth/register`, {
     method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(signupData),  // Send user data to backend
+    body: JSON.stringify(signupData), // Send user data to backend
   });
 
   if (!response.ok) {
     const error = await response.json();
-    throw new Error(error.message || 'Registration failed');
+    throw new Error(error.message || "Registration failed");
   }
 
   return await response.json();
 }
 
-
-
-
 const determineContentType = (key) => {
-  const extension = key.split('.').pop().toLowerCase();
+  const extension = key.split(".").pop().toLowerCase();
   switch (extension) {
-    case 'jpg':
-    case 'jpeg':
-    case 'png':
-    case 'avif':
-    case 'gif':
-      return `image/${extension === 'jpg' ? 'jpeg' : extension}`;
-    case 'mp4':
-      return 'video/mp4';
-    case 'mov':
-      return 'video/quicktime';
-    case 'pdf':
-      return 'application/pdf';
+    case "jpg":
+    case "jpeg":
+    case "png":
+    case "avif":
+    case "gif":
+      return `image/${extension === "jpg" ? "jpeg" : extension}`;
+    case "mp4":
+      return "video/mp4";
+    case "mov":
+      return "video/quicktime";
+    case "pdf":
+      return "application/pdf";
     default:
-      return 'application/octet-stream'; // Default fallback
+      return "application/octet-stream"; // Default fallback
   }
 };
 
-// Sends call to backend API w/ file to upload
-export const uploadFileToS3 = async (file) => {
+// Sends call to backend API w/ file and key for upload
+export const uploadFileToS3 = async (file, key) => {
   if (!file) {
     throw new Error("No file selected");
   }
 
+  if (!key) {
+    throw new Error("No key provided");
+  }
+
   const formData = new FormData();
+  formData.append("key", key);
   formData.append("file", file);
 
   const res = await fetch(`${BACKEND_URL}v1/test/upload`, {
