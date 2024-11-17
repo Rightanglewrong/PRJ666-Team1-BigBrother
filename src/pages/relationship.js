0efc15e635d3bd76b2426781b41a57f5c4184c67
JustinWHLEE16 <jlee722@myseneca.ca>
2024-11-17 03:28:11 -0500

    route fix

--- src/pages/relationship.js
+++ src/pages/relationship.js
@@ -7,7 +7,7 @@ import {
   getRelationshipByChildID,
   getRelationshipByParentID
 } from '../utils/relationshipAPI'; 
-import styles from "../crudTester.module.css";
+import styles from "./crudTester.module.css";
 
 export default function RelationshipCrudTest() {
   const [relationship, setRelationship] = useState({});
