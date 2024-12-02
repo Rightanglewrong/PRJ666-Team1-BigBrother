e0963e9a2270aec1bee8df08e0a54d4a8f562efa
Rightanglewrong <bennyyang996648@hotmail.com>
2024-12-01 22:01:03 -0500

    reformated emailAPI

--- src/utils/emailAPI.js
+++ src/utils/emailAPI.js
@@ -1,27 +1,21 @@
 // src/utils/emailAPI
 
-const BACKEND_URL = "https://big-brother-be-3d6ad173758c.herokuapp.com/smail";
+const BACKEND_URL = 'https://big-brother-be-3d6ad173758c.herokuapp.com/smail';
 
 // Function to send emails based on account type and locationID
-export const sendEmailsToUsers = async (
-  token,
-  accountType,
-  locationID,
-  subject,
-  message
-) => {
+export const sendEmailsToUsers = async (token, accountType, locationID, subject, message) => {
   try {
     const response = await fetch(`${BACKEND_URL}/sendEmails`, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
         Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify({ accountType, locationID, subject, message }),
     });
 
     if (!response.ok) {
-      throw new Error("Failed to send emails");
+      throw new Error('Failed to send emails');
     }
 
     return await response.json();
